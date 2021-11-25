51487ac1901cd2d1c5c2f6ce3cbd0a590318fac1
Bernhard Lukassen <bl@bernhard-lukassen.com>
2021-11-25 10:14:32 +0100

    producer/consumer test

--- test/consumer.mjs
+++ test/consumer.mjs
@@ -6,6 +6,31 @@
  * @see: {@link https://github.com/Thoregon}
  */
 
-export default class Consumer {
+import Facade           from "/thoregon.crystalline/lib/facade.mjs";
+import ThoregonConsumer from "/thoregon.crystalline/lib/providers/thoregonconsumer.mjs";
 
-}
+import { doAsync, timeout } from "/evolux.universe";
+
+debugger;
+
+console.log('\nConsumer start\n');
+
+universe.qtest = {};
+const srvroot = "yPqTS97ESjGK5FV2rCDQqOoP30odvGKS";
+
+const consumer = await Facade.use(await ThoregonConsumer.at(srvroot));
+universe.qtest.consumer = consumer;
+
+consumer.subscribe('change', (evt) => {
+    console.log('Producer -> change', evt);
+});
+
+console.log('Producer.echo()', await consumer.echo('wer ruft in den Wald'));
+console.log('Producer.a = ', await consumer.a);
+
+consumer.a = 'A';
+
+await timeout(100);
+consumer.close();
+
+console.log('\nConsumer end');
--- test/producer.mjs
+++ test/producer.mjs
@@ -6,6 +6,23 @@
  * @see: {@link https://github.com/Thoregon}
  */
 
+import ThoregonProducer from "/thoregon.crystalline/lib/services/thoregonproducer.mjs";
+
 export default class Producer {
 
+    constructor() {
+        this.name = 'Producer';
+    }
+
+    echo(text) {
+        console.log("Producer.echo()", text);
+        return "Got: '" + text + "'";
+    }
+
 }
+
+universe.qtest = {};
+const srvroot = "yPqTS97ESjGK5FV2rCDQqOoP30odvGKS";
+
+const producer = await ThoregonProducer.with(srvroot, new Producer());
+universe.qtest.producer = producer;
