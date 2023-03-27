dc024e2b8ede7494602355352380d840d4e50bba
Bernhard Lukassen <bl@bernhard-lukassen.com>
2023-03-27 14:05:44 +0200

    fixes

--- universe.config.mjs
+++ universe.config.mjs
@@ -13,6 +13,7 @@ export { path }                          from '/evolux.util';
 // import TESTIDENTITY                      from "./testidentity.mjs";
 
 export { default as myagents }           from './agent_config.mjs';
+export { default as KNOWN_PEERS }        from './knownpeers.mjs';
 
 export const DEBUG = false;
 export const HALT  = true;
@@ -22,8 +23,6 @@ export const HALT  = true;
  */
 
 export const NEULAND_STORAGE_OPT = { store: 'data', name: 'neuland' };    // can override: writeCount, writeInterval
-
-export const KNOWN_PEERS = ['PeerJS-Ybeew3NiFgXRT1uu3eIrauxLSpLKSZ51'];
 
 // export const gunpeers =    ['http://185.11.139.203:8765/gun', /* 'http://127.0.0.1:8765/gun' , 'https://matter.thoregon.io:8765/gun'*/];
 
