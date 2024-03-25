198404bd62115e209673b2bd2032895819bd9a5d
Bernhard Lukassen <bl@bernhard-lukassen.com>
2024-03-25 14:16:00 +0100

    portal service for each vendor

--- etc/universe.config.mjs
+++ etc/universe.config.mjs
@@ -18,6 +18,8 @@ export const specials = pspecials;
 export { default as KNOWN_PEERS }        from './knownpeers.mjs';
 export { default as HTTPFILESINK }       from './resourcesink.mjs';
 
+export { default as services }           from './services.mjs';
+
 export const DEBUG = false;
 export const HALT  = true;
 
