02f1b0da67998bef305596fd2c52987ba24ce3f4
Bernhard Lukassen <git@bernhard-lukassen.com>
2024-05-03 21:12:28 +0200

    redirect from iframe

--- relay.mjs
+++ relay.mjs
@@ -8,6 +8,32 @@
 
 import Universe from "./lib/universe.mjs";
 
+function redirectPage(evt) {
+    const url = evt.data.url;
+    if (url) window.location.replace(url);
+}
+
+function init() {
+    window.addEventListener('message', (evt) => received(evt));
+}
+
+function received(evt) {
+    const data = evt.data;
+    if (!data) return;
+    const type = data.type;
+    if (!type) return;
+    switch (type) {
+        case 'redirect':
+            redirectPage(evt);
+    }
+}
+
+try {
+    init();
+} catch (e) {
+    console.error(e);
+}
+
 (async () => {
     const iframe = document.querySelector('iframe');
     if (!iframe) {
